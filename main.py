dfbe333bf39e11f84f1e3996150345d660383925
Matthew Ching Ting Ching <84363861+Matttttt11@users.noreply.github.com>
2021-06-12 06:29:59 +0000

    Redo the function that prevent the user does not enter it name on the box and does not select any options on the question.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from os import makedirs
 from json import dump
 
 name_list = []
-score = 0
+
 
 qanswers = {
     1: [
@@ -16,14 +16,13 @@ qanswers = {
         'Awesome', 'It is fine',
         'The teacher needs to support more students', 'Others',
     ],
-    3: ["What thing does the school need to improve?" ,
+    3: ["What thing does the school need to improve?",
         'School facilities and equipment', 'School environment', 'Others', ],
 
     4: ["How is your friend or your classmate at school?:", 'Great', 'Not bad',
         'I do not have friends at school', 'Other', ],
 
     5: ["Do you think is hard for you to learn the subject you choose this year?:", 'No', 'A' ' bit', 'Yes', 'Other', ],
-
 
     6: ["Does the school private you a good equipment for your study?", 'Yes', 'No', 'Not bad', ],
 
@@ -33,14 +32,14 @@ qanswers = {
 
 class Quizstarter:
     def __init__(self, parent):
-        self.data={}
+        self.data = {}
         self.parent = parent
         self.name = None
         self.number = 0
 
         self.name_page()
 
-    def name_page(self):
+    def name_page(self, check=False):
         background_color = "OldLace"
         color_for_button = "#e3edf6"
         # frame setup
@@ -49,71 +48,71 @@ class Quizstarter:
                                 padx=100,
                                 pady=100)
         self.quiz_frame.grid()
-        self.handing_label = Label(self.quiz_frame,
+        handing_label = Label(self.quiz_frame,
                                    text="Welcome to the Quiz",
                                    bg=background_color)
-        self.handing_label.grid(row=0, padx=20)
+        handing_label.grid(row=0, padx=30)
 
-    
-
-        self.user_label = Label(self.quiz_frame,
+        if check:
+            user_label = Label(self.quiz_frame,
+                               text="Please enter your name:",
+                               font=("Indie Flower", "17"), fg="Red",
+                               bg=background_color)
+        else:
+            user_label = Label(self.quiz_frame,
                                 text="Enter your name:",
                                 font=("Indie Flower", "17"),
                                 bg=background_color)
-        self.user_label.grid(row=2, padx=30, pady=20)
+        user_label.grid(row=2, padx=30, pady=20)
 
         self.entry_box = Entry(self.quiz_frame)
-        self.entry_box.grid(row=3, padx=20, pady=20)
+        self.entry_box.grid(row=3, padx=30, pady=20)
 
-        self.continue_button = Button(self.quiz_frame,
+        continue_button = Button(self.quiz_frame,
                                       text='Continue',
                                       font=("Indie Flower", "13", "bold"),
                                       bg=color_for_button,
                                       command=self.name_collection)
-        self.continue_button.grid(row=4, padx=20, pady=25)
+        continue_button.grid(row=4, padx=20, pady=25)
 
     def end_page(self):
         background_color = "OldLace"
+        color_for_button = "#e3edf6"
         # frame setup
         self.quiz_frame = Frame(self.parent,
                                 bg=background_color,
                                 padx=100,
                                 pady=100)
         self.quiz_frame.grid()
-        self.handing_label = Label(self.quiz_frame,
+        handing_label = Label(self.quiz_frame,
                                    text="Thanks for your responses",
                                    bg=background_color)
-        self.handing_label.grid(row=0, padx=20)
+        handing_label.grid(row=0, padx=20)
 
-        self.continue_button = Button(self.quiz_frame,
+        continue_button = Button(self.quiz_frame,
                                       text='Exit',
                                       font=("Helvetica", "13", "bold"),
                                       bg=color_for_button,
                                       command=exit)
-        self.continue_button.grid(row=4, padx=20, pady=25)
+        continue_button.grid(row=4, padx=20, pady=25)
 
     def name_collection(self):
         self.name = self.entry_box.get()
-        if self.name.replace(' ','') == '':
+        if self.name.replace(' ', '') == '':
             self.quiz_frame.destroy()
-            self.name_page()
-            background_color = "OldLace"
-            self.warning_label = Label(self.quiz_frame,
-                                text="Please enter your name:",
-                                font=("Indie Flower", "15"), fg="Red",
-                                bg=background_color)
-            self.warning_label.grid(row=2, padx=20, pady=20)
-
-          
+            self.name_page(True)
         else:
             self.new_question()
 
-    def new_question(self):
+    def new_question(self, check=False):
+        if not check:
+            self.number += 1
+
         self.quiz_frame.destroy()
-        self.number += 1
         if self.number > len(qanswers):
-            dump(self.data, open(f'data/{self.name}.json','w'))
+            dump(self.data, open(f'data/{self.name}.json', 'w'))
             self.end_page()
+            return
         background_color = "OldLace"
         color_for_button = "#e3edf6"
         # frame setup
@@ -123,47 +122,38 @@ class Quizstarter:
                                 pady=100)
         self.quiz_frame.grid()
 
-        self.question_label = Label(self.quiz_frame, text=qanswers[self.number][0], font=("Tw Cen MT", "16"),
+        question_label = Label(self.quiz_frame, text=qanswers[self.number][0], font=("Tw Cen MT", "16"),
                                     bg=background_color)
-        self.question_label.grid(row=1, padx=10, pady=10)
+        question_label.grid(row=1, padx=10, pady=10)
 
         self.var1 = IntVar()
-
-        self.rb1 = Radiobutton(self.quiz_frame, text=qanswers[self.number][1], font=("Helvetica", "12"),
+        for i in range(1,len(qanswers[self.number])):
+            rb = Radiobutton(self.quiz_frame, text=qanswers[self.number][i], font=("Helvetica", "12"),
                                bg=background_color,
-                               value=1, padx=10, pady=10, variable=self.var1, indicator=0, background=color_for_button)
-        self.rb1.grid(row=2, sticky=W)
+                               value=i, padx=10, pady=10, variable=self.var1, indicator=0, background=color_for_button)
+            rb.grid(row=i+1, sticky=W)
 
-        self.rb2 = Radiobutton(self.quiz_frame, text=qanswers[self.number][2], font=("Helvetica", "12"),
-                               bg=background_color,
-                               value=2, padx=10, pady=10, variable=self.var1, indicator=0, background=color_for_button)
-        self.rb2.grid(row=3, sticky=W)
-
-        self.rb3 = Radiobutton(self.quiz_frame, text=qanswers[self.number][3], font=("Helvetica", "12"),
-                               bg=background_color,
-                               value=3, padx=10, pady=10, variable=self.var1, indicator=0, background=color_for_button)
-        self.rb3.grid(row=4, sticky=W)
-
-        self.quiz_instance = Button(self.quiz_frame, text="Confirm", font=("Helvetica", "13", "bold"),
+        quiz_instance = Button(self.quiz_frame, text="Confirm", font=("Helvetica", "13", "bold"),
                                     bg=color_for_button, command=self.test_progress)
-        self.quiz_instance.grid(row=7, padx=5, pady=5)
+        quiz_instance.grid(row=7, padx=5, pady=5)
 
-    def qsetup(self):
-        self.var1.set(0)
-        self.question_label.config(text=qanswers[self.number][0])
-        self.rb1.config(text=qanswers[self.number][1])
-        self.rb2.config(text=qanswers[self.number][2])
-        self.rb3.config(text=qanswers[self.number][3])
+        if check:
+            label = Label(
+                self.quiz_frame,
+                text="Place chose one",
+                font=("Tw Cen MT", "16"),
+                bg=background_color
+            )
+            label.grid(row=8, padx=10, pady=10)
 
     def test_progress(self):
         choice = self.var1.get()
-        self.quiz_instance.config(text="Confirm")
-        self.data[qanswers[self.number][0]]=qanswers[self.number][choice]
-        self.new_question()
+        self.data[qanswers[self.number][0]] = qanswers[self.number][choice]
+        self.new_question(choice == 0)
 
 
 root = Tk()
 root.title("Quiz")
-makedirs('data',exist_ok=True)
+makedirs('data', exist_ok=True)
 quiz_instance = Quizstarter(root)
 root.mainloop()  # Keep showing the window until we close it.
