a3b16e5efcf6aa0dca6f503df410dcd64845aecc
Matthew Ching Ting Ching <84363861+Matttttt11@users.noreply.github.com>
2021-06-17 02:59:58 +0000

    Set up a button in a question page to prevent the user makes a mistake when he is choosing the options. Set up a condition to prevent the user type the name over 10 characters.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from tkinter import *
+from tkinter.font import families
+
 from numpy.random import shuffle
 from os import makedirs
 from json import dump
 
 name_list = []
-
 
 qanswers = {
     1: [
@@ -39,8 +40,8 @@ class Quizstarter:
 
         self.name_page()
 
-    def name_page(self, check=False):
-        background_color = "OldLace"
+    def name_page(self, check=False,message=''):
+        background_color = "#f4eee2"
         color_for_button = "#e3edf6"
         # frame setup
         self.quiz_frame = Frame(self.parent,
@@ -49,8 +50,8 @@ class Quizstarter:
                                 pady=100)
         self.quiz_frame.grid()
         handing_label = Label(self.quiz_frame,
-                                   text="Welcome to the Quiz",
-                                   bg=background_color)
+                              text="Welcome to the Quiz",
+                              bg=background_color)
         handing_label.grid(row=0, padx=30)
 
         if check:
@@ -60,20 +61,34 @@ class Quizstarter:
                                bg=background_color)
         else:
             user_label = Label(self.quiz_frame,
-                                text="Enter your name:",
-                                font=("Indie Flower", "17"),
-                                bg=background_color)
+                               text="Enter your name:",
+                               font=("Indie Flower", "17"),
+                               bg=background_color)
         user_label.grid(row=2, padx=30, pady=20)
 
-        self.entry_box = Entry(self.quiz_frame)
+        self.entry_box = Entry(self.quiz_frame,textvariable=self.name)
         self.entry_box.grid(row=3, padx=30, pady=20)
 
+        def keypass(event):
+            if event.keycode == 13:
+                self.name_collection()
+
+        self.entry_box.bind('<Key>', keypass)
+
         continue_button = Button(self.quiz_frame,
-                                      text='Continue',
-                                      font=("Indie Flower", "13", "bold"),
-                                      bg=color_for_button,
-                                      command=self.name_collection)
+                                 text='Continue',
+                                 font=("Indie Flower", "13", "bold"),
+                                 bg=color_for_button,
+                                 command=self.name_collection)
         continue_button.grid(row=4, padx=20, pady=25)
+        if message!='':
+            message=Label(self.quiz_frame,
+                  text=message,
+                  font=("Indie Flower", "17"),
+                  fg='red',
+                  bg=background_color)
+            message.grid(row=5)
+
 
     def end_page(self):
         background_color = "OldLace"
@@ -85,15 +100,15 @@ class Quizstarter:
                                 pady=100)
         self.quiz_frame.grid()
         handing_label = Label(self.quiz_frame,
-                                   text="Thanks for your responses",
-                                   bg=background_color)
+                              text="Thanks for your responses",
+                              bg=background_color)
         handing_label.grid(row=0, padx=20)
 
         continue_button = Button(self.quiz_frame,
-                                      text='Exit',
-                                      font=("Helvetica", "13", "bold"),
-                                      bg=color_for_button,
-                                      command=exit)
+                                 text='Exit',
+                                 font=("Helvetica", "13", "bold"),
+                                 bg=color_for_button,
+                                 command=exit)
         continue_button.grid(row=4, padx=20, pady=25)
 
     def name_collection(self):
@@ -101,6 +116,9 @@ class Quizstarter:
         if self.name.replace(' ', '') == '':
             self.quiz_frame.destroy()
             self.name_page(True)
+        elif len(self.name)>10:
+            self.quiz_frame.destroy()
+            self.name_page(message='Do not enter more than 10 characters.')
         else:
             self.new_question()
 
@@ -123,19 +141,28 @@ class Quizstarter:
         self.quiz_frame.grid()
 
         question_label = Label(self.quiz_frame, text=qanswers[self.number][0], font=("Tw Cen MT", "16"),
-                                    bg=background_color)
+                               bg=background_color)
         question_label.grid(row=1, padx=10, pady=10)
 
         self.var1 = IntVar()
-        for i in range(1,len(qanswers[self.number])):
+        for i in range(1, len(qanswers[self.number])):
             rb = Radiobutton(self.quiz_frame, text=qanswers[self.number][i], font=("Helvetica", "12"),
-                               bg=background_color,
-                               value=i, padx=10, pady=10, variable=self.var1, indicator=0, background=color_for_button)
-            rb.grid(row=i+1, sticky=W)
+                             bg=background_color,
+                             value=i, padx=10, pady=10, variable=self.var1, indicator=0, background=color_for_button)
+            rb.grid(row=i + 1, sticky=W)
 
         quiz_instance = Button(self.quiz_frame, text="Confirm", font=("Helvetica", "13", "bold"),
-                                    bg=color_for_button, command=self.test_progress)
+                               bg=color_for_button, command=self.test_progress)
         quiz_instance.grid(row=7, padx=5, pady=5)
+
+        def back():
+            self.number -= 2
+            self.new_question()
+
+        if self.number > 1:
+            back_button = Button(self.quiz_frame, text="Back", font=("Helvetica", "13", "bold"),
+                                 bg=color_for_button, command=back)
+            back_button.grid(row=8, padx=5, pady=5)
 
         if check:
             label = Label(
@@ -150,6 +177,7 @@ class Quizstarter:
         choice = self.var1.get()
         self.data[qanswers[self.number][0]] = qanswers[self.number][choice]
         self.new_question(choice == 0)
+
 
 
 root = Tk()
